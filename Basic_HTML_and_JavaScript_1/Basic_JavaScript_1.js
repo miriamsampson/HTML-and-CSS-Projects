3e8069affb810de68f9e57dc570b976736775d4a
miriamsampson <sumayahsampson@gmail.com>
2025-03-03 23:52:42 -0500

    Basic HTML and JavaScript Folder

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html>
+<script>alert("Hello You!"); 
+function my_first_function(){
+var string="kiss me, I'm Irish!";
+var result=string.font color("green");
+document.getElementById("Irish").innerhtml=result;}
+</script>
+</html> 
---
+++ Basic_HTML_and_JavaScript_1/Hello_World.js
@@ -0,0 +1,6 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<script>
+alert("Hello, World! I am an alert box");
+</script>
+</html>
